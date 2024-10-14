ab26dd61c61f91ab2188a63dc8bfb86d1cf89a3a
404 <12112909+segmented@users.noreply.github.com>
2024-10-14 19:00:09 +0800

    Update v1.py

--- v1.py
+++ v1.py
@@ -6,6 +6,8 @@ import requests
 
 class Pipeline:
     class Valves(BaseModel):
+        # AiHubMix DOC: https://doc.aihubmix.com
+        AIHUBMAX_API_BASE_URL: str = "https://aihubmix.com/v1"
         AIHUBMAX_API_KEY: str = ""
         pass
 
@@ -18,6 +20,8 @@ class Pipeline:
                 )
             }
         )
+
+        self.pipelines = self.get_aihubmix_models()
         pass
 
     async def on_startup(self):
@@ -30,6 +34,55 @@ class Pipeline:
         print(f"on_shutdown:{__name__}")
         pass
 
+    async def on_valves_updated(self):
+        # This function is called when the valves are updated.
+        print(f"on_valves_updated:{__name__}")
+        self.pipelines = self.get_aihubmix_models()
+        pass
+
+    async def on_startup(self):
+        # This function is called when the server is started.
+        print(f"on_startup:{__name__}")
+        pass
+
+    async def on_shutdown(self):
+        # This function is called when the server is stopped.
+        print(f"on_shutdown:{__name__}")
+        pass
+        
+    def get_aihubmix_models(self):
+        if self.valves.AIHUBMAX_API_KEY:
+            try:
+                headers = {}
+                headers["Authorization"] = f"Bearer {self.valves.AIHUBMAX_API_KEY}"
+                headers["Content-Type"] = "application/json"
+
+                r = requests.get(
+                    f"{self.valves.AIHUBMAX_API_BASE_URL}/models", headers=headers
+                )
+
+                models = r.json()
+                return [
+                    {
+                        "id": model["id"],
+                        "name": model["name"] if "name" in model else model["id"],
+                    }
+                    for model in models["data"]
+                    if "gpt" in model["id"]
+                ]
+
+            except Exception as e:
+
+                print(f"Error: {e}")
+                return [
+                    {
+                        "id": "error",
+                        "name": "Could not fetch models from OpenAI, please update the API Key in the valves.",
+                    },
+                ]
+        else:
+            return []
+            
     def pipe(
         self, user_message: str, model_id: str, messages: List[dict], body: dict
     ) -> Union[str, Generator, Iterator]:
